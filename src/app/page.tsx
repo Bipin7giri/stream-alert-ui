9a2c918a8bf402a13c29886596ed3429e8c99a6e
bipin7giri <bipingiri27@gmail.com>
2024-03-17 14:04:17 +0545

    alert box complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+"use client";
+import { Button } from "@material-tailwind/react";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>hi</h1>
+      <Button key={"button"} size="lg" variant="gradient" placeholder={"ok"}>
+        hi
+      </Button>
+      ;
     </main>
   );
 }
--- src/app/streamer/alert-box/page.tsx
+++ src/app/streamer/alert-box/page.tsx
@@ -1,16 +1,41 @@
 "use client";
-import { Button, Form, Input, Modal, Select, Upload } from "antd";
+import {
+  Button,
+  ColorPicker,
+  Form,
+  Input,
+  Modal,
+  Select,
+  Slider,
+  Upload,
+  notification,
+} from "antd";
 import { Option } from "antd/es/mentions";
 import React, { useState } from "react";
 import { UploadOutlined } from "@ant-design/icons";
 import { GIF } from "@/data/gif.data";
 import Image from "next/image";
 import { dev_base_url, img_base_url } from "@/constants/baseurl";
+import { BsFileFont } from "react-icons/bs";
+import { getCookie } from "cookies-next";
+const defaultMessageTemplate =
+  "${alertMessage.username} tipped rs ${alertMessage.amount}";
+function rgbToHex(r: number, g: number, b: number) {
+  return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
 const Page = () => {
+  const [templateUrl, setTemplateUrl] = useState("");
+  const accessToken = getCookie("access_token");
+
+  const [inputValue, setInputValue] = useState(1);
+
+  const onChange = (newValue: number) => {
+    setInputValue(newValue);
+  };
   // modal
   const [image, setImage] = useState<string>("");
   const [isModalOpen, setIsModalOpen] = useState(false);
-
+  const [fontSetting, setFontSetting] = useState(false);
   const showModal = () => {
     setIsModalOpen(true);
   };
@@ -22,16 +47,117 @@ const Page = () => {
   const handleCancel = () => {
     setIsModalOpen(false);
   };
-  const onFinish = () => {};
+  const uploadFile = async (file: File) => {
+    const formData = new FormData();
+    formData.append("gif", file);
+
+    try {
+      const response = await fetch(dev_base_url + "/upload/alert-gif", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (!response.ok) {
+        throw new Error("Upload failed");
+      }
+
+      const responseData = await response.json();
+      setImage(responseData.data);
+      handleOk();
+      console.log("File uploaded successfully:", responseData);
+      // Handle success
+    } catch (error: any) {
+      console.error("Error uploading file:", error.message);
+      // Handle error
+      notification.error({
+        message: "Something went wrong uploading gif",
+      });
+    }
+  };
+  const onFinish = async (value: any) => {
+    const { r, g, b } = value.textColor.metaColor;
+    const hexValue = rgbToHex(Math.round(r), Math.round(g), Math.round(b));
+    const requestBody = {
+      title: value?.messageTemplate
+        ? value.messageTemplate
+        : defaultMessageTemplate,
+      message: "test alert",
+      messageStyle: {
+        font: value.fontFamily,
+        font_size: value.fontSize,
+        text_color: hexValue,
+        font_weight: value.fontWeight,
+      },
+      visibility: {
+        duration: 5,
+        delay: 0,
+      },
+      image: {
+        path: image,
+      },
+      audio: {
+        url: "http://localhost:8000/assets/audio.ogg",
+      },
+      animation: true,
+      custom: {
+        enter_animation: "flash",
+        exit_animation: "fadeOut",
+      },
+    };
+    try {
+      const response = await fetch(dev_base_url + "/templates", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        } as HeadersInit,
+        body: JSON.stringify(requestBody),
+      });
+
+      if (!response.ok) {
+        throw new Error("Upload failed");
+      }
+
+      const responseData = await response.json();
+      setTemplateUrl(responseData.data);
+      handleOk();
+      console.log("File uploaded successfully:", responseData);
+      // Handle success
+    } catch (error: any) {
+      console.error("Error uploading file:", error.message);
+      // Handle error
+      notification.error({
+        message: "Something went wrong uploading gif",
+      });
+    }
+  };
+  const copyTemplateUrl = () => {
+    // Copy the content of the templateUrl div to the clipboard
+    navigator.clipboard
+      .writeText(templateUrl)
+      .then(() => {
+        alert("Copied: " + templateUrl);
+      })
+      .catch((error) => {
+        console.error("Failed to copy:", error);
+      });
+  };
   return (
     <div>
       <div className="bg-background p-8 rounded-xl">
         <div className="flex items-center justify-around">
-          <h1>Alert Box</h1>
+          <h1 className="text-3xl font-bold">Alert Box</h1>
           <div className="flex gap-4 items-center">
-            <Input defaultValue={"http://localhost:8000"} />
+            <div className="w-[400px] h-[41px] bg-gray-200 p-2 rounded ">
+              {templateUrl}
+            </div>
+
             <div className="flex justify-between gap-3 items-center">
-              <Button size="large" className="bg-primary text-white">
+              <Button
+                onClick={copyTemplateUrl}
+                size="large"
+                className="bg-primary text-white"
+              >
                 Copy
               </Button>
               <Button size="large" className="bg-primary-2">
@@ -49,52 +175,131 @@ const Page = () => {
             label="Message Template"
             name="messageTemplate"
             className="font-manrope font-semibold text-[13px] col-span-1"
-            rules={[
-              {
-                required: true,
-                message: "Please enter your message template!",
-              },
-            ]}
           >
             <Input
-              defaultValue={
-                "${alertMessage.username} tipped rs ${alertMessage.amount}"
-              }
+              defaultValue={defaultMessageTemplate}
               placeholder="Full Name"
-              className="py-2"
+              className="py-2 focus:border-black hover:border-black"
             />
           </Form.Item>
           <Form.Item
             label="Text Animation"
             name="textAnimation"
-            className="font-manrope font-semibold text-[13px] col-span-1"
-            rules={[
-              { required: true, message: "Please enter your full name!" },
-            ]}
+            className="font-manrope font-semibold text-[13px] col-span-1 focus:border-black hover:border-black"
+            rules={[{ required: true, message: "Please add animation!" }]}
           >
-            <Select placeholder="Text Animation" className="w-[100%] h-[41px]">
-              <Option value="">Fade</Option>
-              <Option value="">Fade</Option>
-              <Option value="">Fade</Option>
-              <Option value="">Fade</Option>
-              <Option value="">Fade</Option>
+            <Select
+              placeholder="Text Animation"
+              className="w-[100%] h-[41px] focus:border-black hover:border-black "
+            >
+              <Option value="fade">Fade</Option>
+              <Option value="shakeX">Shake x</Option>
+              <Option value="shakeY">shakeY</Option>
+              <Option value="bounce">Bounce</Option>
+              <Option value="flash">Flash</Option>
             </Select>
           </Form.Item>
           <Form.Item
             label="Image"
             name="image"
-            className="font-manrope font-semibold text-[13px] col-span-1"
-            rules={[{ required: true, message: "Please Upload Image" }]}
+            className="font-manrope font-semibold text-[13px] col-span-2"
           >
             <div
               onClick={() => {
                 showModal();
               }}
             >
-              <div className="w-full">
+              <div className="w-full  flex items-center gap-8">
                 <Button icon={<UploadOutlined />}>Upload</Button>
+                {image && (
+                  <div>
+                    <img
+                      height={100}
+                      className="w-[100px] h-[100px]"
+                      width={100}
+                      alt="image"
+                      src={image}
+                    />
+                  </div>
+                )}
               </div>
             </div>
+          </Form.Item>
+
+          <Form.Item
+            label="Font Family"
+            name="fontFamily"
+            rules={[
+              { required: true, message: "Please input your font family!" },
+            ]}
+          >
+            <Select className="h-[41px] focus:border-black hover:border-black">
+              <Select.Option value="roboto">Roboto</Select.Option>
+              <Select.Option value="openSans">Open Sans</Select.Option>
+              <Select.Option value="lato">Lato</Select.Option>
+              <Select.Option value="poppins">Poppins</Select.Option>
+            </Select>
+          </Form.Item>
+
+          <Form.Item
+            label="Font Size"
+            name="fontSize"
+            rules={[
+              { required: true, message: "Please input your font size!" },
+            ]}
+          >
+            <Slider
+              styles={{
+                handle: {
+                  background: "black",
+                  color: "black",
+                },
+                track: {
+                  background: "black",
+                },
+              }}
+              min={16}
+              max={80}
+              onChange={onChange}
+              value={typeof inputValue === "number" ? inputValue : 0}
+            />
+          </Form.Item>
+
+          <Form.Item
+            label="Font Weight"
+            name="fontWeight"
+            rules={[
+              { required: true, message: "Please input your font weight!" },
+            ]}
+          >
+            <Select className="h-[41px] focus:border-black hover:border-black">
+              <Select.Option value="400">400</Select.Option>
+              <Select.Option value="500">500</Select.Option>
+              <Select.Option value="600">600</Select.Option>
+              <Select.Option value="700">700</Select.Option>
+              <Select.Option value="800">800</Select.Option>
+            </Select>
+          </Form.Item>
+
+          <Form.Item
+            label="Text Color"
+            name="textColor"
+            className="h-[41px]"
+            rules={[
+              { required: true, message: "Please input your text color!" },
+            ]}
+          >
+            <ColorPicker format="hex" defaultValue="black" />
+          </Form.Item>
+
+          <Form.Item className="col-span-2 flex justify-end">
+            <Button
+              className="bg-primary text-white"
+              type="text"
+              htmlType="submit"
+            >
+              Submit
+            </Button>
           </Form.Item>
         </Form>
       </div>
@@ -112,7 +317,7 @@ const Page = () => {
               return (
                 <div
                   onClick={() => {
-                    setImage(item);
+                    setImage(img_base_url + item);
                     handleCancel();
                   }}
                   className="bg-gray-500 rounded-xl hover:shadow-lg hover:cursor-pointer"
@@ -130,7 +335,17 @@ const Page = () => {
             })}
           </div>
           <div className="flex justify-end">
-            <Upload className="w-full">
+            <Upload
+              customRequest={({ file, onSuccess, onError }: any) => {
+                uploadFile(file as File).then(() => {
+                  onSuccess();
+                  notification.info({
+                    message: "Gif Uploaded Successfully",
+                  });
+                });
+              }}
+              className="w-full"
+            >
               <Button icon={<UploadOutlined />}>Upload Yours</Button>
             </Upload>
           </div>
